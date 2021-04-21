cad88bb46c1c1cc7c737c478da3acb5c8553a6d0
Baeerd <1084415787@qq.com>
2021-04-21 10:31:04 +0800

    数据库修改

--- src/main/resources/db/migration/V1.1__data.sql
+++ src/main/resources/db/migration/V1.1__data.sql
@@ -1,78 +1,161 @@
-DROP TABLE IF EXISTS DATA_CONFIG;
-CREATE TABLE DATA_CONFIG (
-  ID bigint NOT NULL AUTO_INCREMENT,
-  CREATED_DT datetime NULL,
-  CREATED_BY varchar(32) NULL,
-  NAME varchar(16) NULL,
-  VALUE varchar(16) NULL,
-  TYPE_ID varchar(16) NULL,
-  PRIMARY KEY (ID)
-);
+USE [forum]
+GO
 
-INSERT INTO `forum-app`.`DATA_CONFIG`(`ID`, `CREATED_DT`, `CREATED_BY`, `NAME`, `VALUE`, `TYPE_ID`) VALUES (1, '2021-04-17 09:04:33', 'admin', '男', '1', 'sex');
-INSERT INTO `forum-app`.`DATA_CONFIG`(`ID`, `CREATED_DT`, `CREATED_BY`, `NAME`, `VALUE`, `TYPE_ID`) VALUES (2, '2021-04-17 09:04:33', 'admin', '女', '0', 'sex');
-INSERT INTO `forum-app`.`DATA_CONFIG`(`ID`, `CREATED_DT`, `CREATED_BY`, `NAME`, `VALUE`, `TYPE_ID`) VALUES (3, '2021-04-17 09:04:33', 'admin', '大一', '1', 'level');
-INSERT INTO `forum-app`.`DATA_CONFIG`(`ID`, `CREATED_DT`, `CREATED_BY`, `NAME`, `VALUE`, `TYPE_ID`) VALUES (4, '2021-04-17 09:04:33', 'admin', '大二', '2', 'level');
-INSERT INTO `forum-app`.`DATA_CONFIG`(`ID`, `CREATED_DT`, `CREATED_BY`, `NAME`, `VALUE`, `TYPE_ID`) VALUES (5, '2021-04-17 09:04:33', 'admin', '大三', '3', 'level');
-INSERT INTO `forum-app`.`DATA_CONFIG`(`ID`, `CREATED_DT`, `CREATED_BY`, `NAME`, `VALUE`, `TYPE_ID`) VALUES (6, '2021-04-17 09:04:33', 'admin', '大四', '4', 'level');
-INSERT INTO `forum-app`.`DATA_CONFIG`(`ID`, `CREATED_DT`, `CREATED_BY`, `NAME`, `VALUE`, `TYPE_ID`) VALUES (7, '2021-04-17 09:04:33', 'admin', '准毕业生', '1', 'role');
-INSERT INTO `forum-app`.`DATA_CONFIG`(`ID`, `CREATED_DT`, `CREATED_BY`, `NAME`, `VALUE`, `TYPE_ID`) VALUES (8, '2021-04-17 09:04:33', 'admin', '校友', '2', 'role');
+/****** Object:  Table [dbo].[DATA_CONFIG]    Script Date: 04/19/2021 13:10:49 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[DATA_CONFIG](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[CREATED_DT] [datetime2](7) NULL,
+	[CREATED_BY] [varchar](32) NULL,
+	[NAME] [varchar](16) NULL,
+	[VALUE] [varchar](16) NULL,
+	[TYPE_ID] [varchar](16) NULL,
+ CONSTRAINT [_copy_3] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
 
 
-DROP TABLE IF EXISTS TABLE_COMMENT;
-CREATE TABLE TABLE_COMMENT  (
-  ID bigint NOT NULL AUTO_INCREMENT,
-  created_dt datetime NULL COMMENT '创建时间',
-  created_by varchar(32) NULL COMMENT '创建人',
-  POST_ID bigint NULL COMMENT '帖子ID',
-  EVALUATE_CONTENT varchar(255) NULL COMMENT '评价内容',
-  EVALUATE_DT longblob NULL COMMENT '评价时间',
-  user_id bigint NULL COMMENT '人员ID',
-  PRIMARY KEY (ID)
-);
+USE [forum]
+GO
 
-DROP TABLE IF EXISTS TABLE_POST;
-CREATE TABLE TABLE_POST  (
-  id bigint NOT NULL AUTO_INCREMENT,
-  CREATED_DT datetime NULL COMMENT '创建时间',
-  CREATED_BY varchar(32) NULL COMMENT '创建人',
-  POST_CODE varchar(32) NULL COMMENT '帖子ID',
-  POST_TITLE varchar(128) NULL COMMENT '帖子标题',
-  POST_CONTENT text NULL COMMENT '帖子主体内容',
-  POST_FILE varchar(50) NULL COMMENT '帖子附件',
-  browse_COUNT int NULL COMMENT '浏览次数',
-  Boutique varchar(1) NULL COMMENT '是否精品',
-  PRIMARY KEY (id)
-);
+/****** Object:  Table [dbo].[table_comment]    Script Date: 04/19/2021 13:11:39 ******/
+SET ANSI_NULLS ON
+GO
 
-INSERT INTO TABLE_POST(id, CREATED_DT, CREATED_BY, POST_CODE, POST_TITLE, POST_CONTENT, POST_FILE, browse_COUNT, Boutique) VALUES (1, '2021-04-16 10:11:02', 'admin', '111', '测试标题111', '测试内容111', '111.zip', 0, NULL);
-INSERT INTO TABLE_POST(id, CREATED_DT, CREATED_BY, POST_CODE, POST_TITLE, POST_CONTENT, POST_FILE, browse_COUNT, Boutique) VALUES (2, '2021-04-16 10:11:11', 'admin', '222', '测试标题222', '测试内容222', '222.zip', 0, NULL);
+SET QUOTED_IDENTIFIER ON
+GO
 
-DROP TABLE IF EXISTS TABLE_USER;
-CREATE TABLE TABLE_USER  (
-  id bigint NOT NULL AUTO_INCREMENT,
-  CREATED_DT datetime NULL COMMENT '创建时间',
-  CREATED_BY varchar(32) NULL COMMENT '创建人',
-  USERNAME varchar(32) NULL COMMENT '用户名',
-  PASSWORD varchar(32) NULL COMMENT '密码',
-  NAME varchar(20) NULL COMMENT '姓名',
-  STUDY_CODE varchar(50) NULL COMMENT '学号',
-  SEX varchar(1) NULL COMMENT '性别',
-  LEVEL varchar(10) NULL COMMENT '年级',
-  subject varchar(128) NULL COMMENT '专业',
-  PHONE varchar(50) NULL COMMENT '联系方式',
-  portrait varchar(50) NULL COMMENT '头像',
-  remark varchar(255) NULL COMMENT '简介',
-  role varchar(1) NULL COMMENT '角色',
-  PRIMARY KEY (id)
-);
+SET ANSI_PADDING ON
+GO
 
-INSERT INTO `forum-app`.`TABLE_USER`(`id`, `CREATED_DT`, `CREATED_BY`, `USERNAME`, `PASSWORD`, `NAME`, `STUDY_CODE`, `SEX`, `LEVEL`, `subject`, `PHONE`, `portrait`, `remark`, `role`) VALUES (1, '2021-04-19 09:11:40', 'admin', 'admin', '123', '管理员', 'xxxxxxxx', '1', '1', 'xx专业', '88888888', '/image/user/admin.png', '系统管理员', '0');
+CREATE TABLE [dbo].[table_comment](
+	[ID] [bigint] IDENTITY(1,1) NOT NULL,
+	[created_dt] [datetime2](7) NULL,
+	[created_by] [varchar](32) NULL,
+	[POST_ID] [bigint] NULL,
+	[EVALUATE_CONTENT] [varchar](255) NULL,
+	[EVALUATE_DT] [varbinary](max) NULL,
+	[user_id] [bigint] NULL,
+ CONSTRAINT [_copy_2] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
 
-create or REPLACE view VIEW_POST as 
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
+USE [forum]
+GO
+
+/****** Object:  Table [dbo].[table_post]    Script Date: 04/19/2021 13:11:51 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[table_post](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[CREATED_DT] [datetime2](7) NULL,
+	[CREATED_BY] [varchar](32) NULL,
+	[POST_CODE] [varchar](32) NULL,
+	[POST_TITLE] [varchar](128) NULL,
+	[POST_CONTENT] [varchar](max) NULL,
+	[POST_FILE] [varchar](50) NULL,
+	[browse_COUNT] [int] NULL,
+	[Boutique] [varchar](1) NULL,
+ CONSTRAINT [_copy_4] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
+USE [forum]
+GO
+
+/****** Object:  Table [dbo].[table_user]    Script Date: 04/19/2021 13:12:03 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [dbo].[table_user](
+	[id] [bigint] IDENTITY(1,1) NOT NULL,
+	[CREATED_DT] [datetime2](7) NULL,
+	[CREATED_BY] [varchar](32) NULL,
+	[USERNAME] [varchar](32) NULL,
+	[PASSWORD] [varchar](32) NULL,
+	[NAME] [varchar](20) NULL,
+	[STUDY_CODE] [varchar](50) NULL,
+	[SEX] [varchar](1) NULL,
+	[LEVEL] [varchar](10) NULL,
+	[subject] [varchar](128) NULL,
+	[PHONE] [varchar](50) NULL,
+	[portrait] [varchar](50) NULL,
+	[remark] [varchar](255) NULL,
+	[role] [nvarchar](1) NULL,
+ CONSTRAINT [_copy_1] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
+INSERT INTO DATA_CONFIG(CREATED_DT, CREATED_BY, NAME, VALUE, TYPE_ID) VALUES ('2021-04-17 09:04:33', 'admin', '男', '1', 'sex');
+INSERT INTO DATA_CONFIG(CREATED_DT, CREATED_BY, NAME, VALUE, TYPE_ID) VALUES ('2021-04-17 09:04:33', 'admin', '女', '0', 'sex');
+INSERT INTO DATA_CONFIG(CREATED_DT, CREATED_BY, NAME, VALUE, TYPE_ID) VALUES ('2021-04-17 09:04:33', 'admin', '大一', '1', 'level');
+INSERT INTO DATA_CONFIG(CREATED_DT, CREATED_BY, NAME, VALUE, TYPE_ID) VALUES ('2021-04-17 09:04:33', 'admin', '大二', '2', 'level');
+INSERT INTO DATA_CONFIG(CREATED_DT, CREATED_BY, NAME, VALUE, TYPE_ID) VALUES ('2021-04-17 09:04:33', 'admin', '大三', '3', 'level');
+INSERT INTO DATA_CONFIG(CREATED_DT, CREATED_BY, NAME, VALUE, TYPE_ID) VALUES ('2021-04-17 09:04:33', 'admin', '大四', '4', 'level');
+INSERT INTO DATA_CONFIG(CREATED_DT, CREATED_BY, NAME, VALUE, TYPE_ID) VALUES ('2021-04-17 09:04:33', 'admin', '准毕业生', '1', 'role');
+INSERT INTO DATA_CONFIG(CREATED_DT, CREATED_BY, NAME, VALUE, TYPE_ID) VALUES ('2021-04-17 09:04:33', 'admin', '校友', '2', 'role');
+
+INSERT INTO TABLE_POST(CREATED_DT, CREATED_BY, POST_CODE, POST_TITLE, POST_CONTENT, POST_FILE, browse_COUNT, Boutique) VALUES ('2021-04-16 10:11:02', 'admin', '111', '测试标题111', '测试内容111', '111.zip', 0, NULL);
+INSERT INTO TABLE_POST(CREATED_DT, CREATED_BY, POST_CODE, POST_TITLE, POST_CONTENT, POST_FILE, browse_COUNT, Boutique) VALUES ('2021-04-16 10:11:11', 'admin', '222', '测试标题222', '测试内容222', '222.zip', 0, NULL);
+
+create view VIEW_POST as 
 SELECT p.*, u.name from TABLE_POST p left join TABLE_USER u on p.CREATED_BY = u.USERNAME;
 
-create or REPLACE view VIEW_USER as
+INSERT INTO TABLE_USER(CREATED_DT, CREATED_BY, USERNAME, PASSWORD, NAME, STUDY_CODE, SEX, LEVEL, subject, PHONE, portrait, remark, role) VALUES ('2021-04-19 09:11:40', 'admin', 'admin', '123', '管理员', 'xxxxxxxx', '1', '1', 'xx专业', '88888888', '/image/user/admin.png', '系统管理员', '0');
+
+create view VIEW_USER as
 SELECT u.*,
 (SELECT c.NAME from DATA_CONFIG c where c.VALUE = u.sex and c.TYPE_ID = 'sex') as sexView,
 (SELECT c.NAME from DATA_CONFIG c where c.VALUE = u.LEVEL and c.TYPE_ID = 'level') as levelView,
